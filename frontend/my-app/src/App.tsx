ae64ffd5b7fa49cd9802354ed2382ca3b8c693e8
Olivia Flynn <oaflynn@syr.edu>
2019-04-27 12:07:07 -0400

    ready to send host info to backend

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,3 +1,6 @@
+// Spotify authorization functionality based on
+// https://levelup.gitconnected.com/how-to-build-a-spotify-player-with-react-in-15-minutes-7e01991bc4b6
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
@@ -5,8 +8,41 @@ import './App.css';
 import SpotifyLogin from './SpotifyLogin';
 import DjangoCalls from './DjangoCalls';
 
+export const authEndpoint = 'https://accounts.spotify.com/authorize?';
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "f694f6f7a1584567948f99d653a9d070";
+const redirectUri = "http://localhost:3000/callback";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-read-email",
+  "user-read-birthdate",
+  "user-follow-modify",
+  "user-follow-read",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "playlist-modify-public",
+  "playlist-modify-private",
+  "user-library-read",
+  "user-library-modify",
+  "user-read-private"
+];
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial: any, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
+
 interface IState {
-  value: string;
+  value: string,
+  token: string
 }
 
 interface IProps {}
@@ -14,24 +50,33 @@ interface IProps {}
 class App extends Component<IProps, IState> {
   constructor(props: any) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      token: '',
+    };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event: any) {
+  componentDidMount = () => {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+
+  handleChange = (event: any) => {
     this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event:any) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
   }
 
   Landing = () => {
     let login = () => {
       let sp = new SpotifyLogin()
+      // When the token comes, move to next page
       let tokenSub = sp.getToken().subscribe(token => console.log(token));
       sp.login();
       setTimeout(() => console.log(sp.token), 15000)
@@ -49,9 +94,18 @@ class App extends Component<IProps, IState> {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <button onClick={login}>Spotify Login</button>
-          <input type='text' value={this.state.value} onChange={this.handleChange}></input>
-          <button onClick={playSong}>Play a song</button>
+          {!this.state.token && (
+            <button>
+              <a href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
+              Spotify Login
+              </a>
+            </button>
+          )}
+          {this.state.token && (
+            <p>{this.state.token}</p>
+          )}
+          {/* <input type='text' value={this.state.value} onChange={this.handleChange}></input>
+          <button onClick={playSong}>Play a song</button> */}
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -65,17 +119,20 @@ class App extends Component<IProps, IState> {
     );
   }
 
-  CallBack() {
+  CallBack = () => {
     let extractToken = () => {
       let token = window.location.hash;
       token = token.substring(token.indexOf("=")+1, token.indexOf("&"));
+      // Send token to backend to create a new room OR add to state?
+      // Close the window
       alert(token);
     }
 
     return(
       <div className="App">
         <h2>Congratualation, you signed into spotify!</h2>
-        <button onClick={extractToken}>Go back to Auxy</button>
+        <input type='text' value={this.state.value} onChange={this.handleChange}></input>
+        <button onClick={extractToken}>party time B-)</button>
       </div>
     )
   }
