e67ba18c087a319b481148fe77154131823266b3
moonmahbubar <moonmahbubar@gmail.com>
2019-04-27 21:50:16 -0400

    Updates

---
+++ precache-manifest.4e8d4414fff858b9cd9f77a99cd6116a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7168d881b0d55b80364a",
+    "url": "/auxy/static/js/runtime~main.6967b179.js"
+  },
+  {
+    "revision": "927b0136e714369b2a20",
+    "url": "/auxy/static/js/main.5a99ac99.chunk.js"
+  },
+  {
+    "revision": "eaa75cccca269448e75c",
+    "url": "/auxy/static/js/2.7384a527.chunk.js"
+  },
+  {
+    "revision": "927b0136e714369b2a20",
+    "url": "/auxy/static/css/main.5e95735f.chunk.css"
+  },
+  {
+    "revision": "a7c5e7918be27d0589e1259f461f1bd3",
+    "url": "/auxy/index.html"
+  }
+];
